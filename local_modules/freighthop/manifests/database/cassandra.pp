8d0a613846a2ed59d01d13e723efdc439aac276c
Nick Cloward <ncloward@instructure.com>
2014-01-08 22:18:24 -0700

    Add cassandra support

--- local_modules/freighthop/manifests/database/cassandra.pp
+++ local_modules/freighthop/manifests/database/cassandra.pp
@@ -1,9 +1,10 @@
 class freighthop::database::cassandra(
+  $package_name  = 'dsc20'
   $cluster_name  = 'freighthop',
-  $seeds         = [ '27.0.0.1', ],
+  $seeds         = [ '127.0.0.1', ],
 ) {
   class { '::cassandra':
-    package_name => "dsc20",
+    package_name => $package_name,
     cluster_name => $cluster_name,
     seeds        => $seeds,
   }
